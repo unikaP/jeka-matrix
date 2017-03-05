97bd20b66d8c063fb8456d3e4bee50ef0ad8ffb1
Iunona Panasenco <Iunona.Panasenco@endava.com>
2017-03-05 13:34:32 +0200

    Initial Commit

---
+++ JekaPatsanDeTreaba/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  JekaPatsanDeTreaba
+//
+//  Created by Iunona Panasenco on 3/5/17.
+//  Copyright © 2017 Iunona Panasenco. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
